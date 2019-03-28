4ebfc447e2792bd80b0ee9d7e3fd3d21a6d051c1
Aviad Ariel <arielaviad1@gmail.com>
2019-03-29 00:04:43 +0200

    Finish schedule section

---
+++ au/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AuConfig(AppConfig):
+    name = 'au'
---
+++ venv/include/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aviad/PycharmProjects/hack/venv/include/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
